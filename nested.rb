d8224f8cfa16cbdd1161f2241d42b37d33b324c2
Eric Winbigler <uzof99@gmail.com>
2019-06-27 14:36:33 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -66,7 +66,7 @@ def adding_matz
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
-
+ programmer_hash[:yukihiro_matsumoto] ={:known_for => "Ruby",:languages => ["LISP, C"]}
  programmer_hash
 
 	programmer_hash = 
@@ -86,7 +86,7 @@ def adding_matz
         
      }
      
-    programmer_hash[:yukihiro_matsumoto] ={:known_for => "Ruby",:languages => ["LISP, C"]}
+   
 end
  
  

b4b089f0726a74c5af50a9f2391668cf62579ce8
Eric Winbigler <uzof99@gmail.com>
2019-06-27 14:47:33 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,16 +1,25 @@
 
 def hopper
-	programmer_hash = 
- 		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          
-        }
-       
-     }
+def hopper	  programmer_hash = 
+	programmer_hash = 	    {
+ 			  :grace_hopper => {
+        :known_for => "COBOL",
+        :languages => ["COBOL", "FORTRAN"]
+          :languages => ["COBOL", "FORTRAN"]	        },
+        },	        :alan_kay => {
+        :alan_kay => {	          :known_for => "Object Orientation",
+          :known_for => "Object Orientation",	          :languages => ["Smalltalk", "LISP"]
+          :languages => ["Smalltalk", "LISP"]	        },
+        },	        :dennis_ritchie => {
+        :dennis_ritchie => {	          :known_for => "Unix",
+          :known_for => "Unix",	          :languages => ["C"]
+          :languages => ["C"]	        }
+        }	     }
+     }	
 
+ 	programmer_hash[:grace_hopper]
 
-end
+ end
 
 
 def alan_kay_is_known_for
